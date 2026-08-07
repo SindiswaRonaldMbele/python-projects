f617daf79cae19febfa00911b86e0bd4a7fa0ee4
Sindiswa Ronald Mbele <sindiswaronald@gmail.com>
2026-08-07 17:15:18 +0200

    Add operational calculator with multiple operations

---
+++ calculator/2_operational_calculator.py
@@ -0,0 +1,14 @@
+x = float(input("First number: "))
+y = float(input("Second number: "))
+operator = input("Operator (+, -, *, /): ")
+
+if operator == "+":
+    result = x + y
+if operator == "-":
+    result = x - y
+if operator == "*":
+    result = x * y
+if operator == "/":
+    result = x / y
+
+print(result)
