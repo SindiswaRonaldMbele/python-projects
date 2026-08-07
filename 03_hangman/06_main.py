4ed44431d7418dec71df2d813331b8aa472aa958
Sindiswa Ronald Mbele <sindiswaronald@gmail.com>
2026-08-07 18:33:41 +0200

    Complete Hangman implementation

---
+++ 03_hangman/06_main.py
@@ -0,0 +1,37 @@
+# Version 6 — Final Polishing
+import random
+
+random_words = ["river", "gentle", "falcon", "beacon", "velvet", "whisper", "anchor", "orbit", "canyon", "zenith", 
+                "ripple", "breeze", "summit", "oasis", "planet", "harmony", "glimmer", "mosaic", "tundra", "vortex"]
+
+secret_word = random.choice(random_words)
+lives = 6
+
+size = len(secret_word)
+
+guessed = "_" * size
+
+while (lives != 0):
+    guess = input("Guess the letter: ")
+    guess = guess.lower()
+    
+    if guess in guessed and guess != "":
+        print("You already guessed that letter.")
+    if guess == "":
+            print("<invalid input>\nCannot take an empty input.")
+            
+    if len(guess) > 1:
+        print("Only one character at a time.")
+    elif guess not in secret_word:
+            lives -= 1
+    
+    for k in range(size):
+        if (guess in secret_word and secret_word[k] == guess):
+            guessed = guessed[:k] + secret_word[k] + guessed[k+1:]
+    print("Lives remaining: ", lives)    
+    print(guessed)
+
+if guessed == secret_word:
+    print("Congratulations!")
+else:
+    print("Game Over!, \nThe word was: ", secret_word)
